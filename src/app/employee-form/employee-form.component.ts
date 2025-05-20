5c7c6e34b5d34fcbe887952a8f2c5d3c9bdebf8c
Dardan Radoniqi <dardan.radoniqi@hotmail.com>
2025-05-20 21:49:13 +0200

    Employee-Form

--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../Models/employee';
+
+
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css'
 })
 export class EmployeeFormComponent {
+  employee: Employee = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    position: ''
+  }
+
+  onSubmit(): void {
+    console.log('Form submitted:', this.employee);
+    // Here you can add the logic to send the employee data to the server
+  }
+
 
 }
